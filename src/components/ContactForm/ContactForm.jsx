97a6eebfd0c373b433b1183b6274f733d9031fa5
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-19 23:06:29 +0300

    update ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,56 +1,61 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { Form, Label, Input, Button } from './ContactForm.styled';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export function ContactForm({onSubmit})  {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  handleChange = event => {
+  const  handleChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      
+      case 'number':
+        setNumber(value);
+        break;
+      
+      default:
+        break;
+    }
   };
 
-  handleSubmit = event => {
+ const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+  onSubmit({ name: name, number: number });
+  reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label htmlFor={this.nameId}>
+      <Form onSubmit={handleSubmit}>
+        <Label>
           Name
           <Input
             type="text"
             name="name"
             value={name}
-            onChange={this.handleChange}
+            onChange={handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
         </Label>
 
-        <Label htmlFor={this.numberId}>
+        <Label>
           Number
           <Input
             type="tel"
             name="number"
             value={number}
-            onChange={this.handleChange}
+            onChange={handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
@@ -59,7 +64,10 @@ export class ContactForm extends Component {
         <Button type="submit">Add contact</Button>
       </Form>
     );
-  }
+}
+
+ContactForm.porTypes = {
+  onSubmit: PropTypes.func.isRequired,
 }
 
 export default ContactForm;
